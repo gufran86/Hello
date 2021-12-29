f574a4d93877285a06f5b5aab96314ddb197b0b7
gufran86 <ahmadghufran900@gmail.com>
2021-12-29 18:47:13 +0500

    Main activity my sy lifecyles methods ura diye hain, aur setOnClick listener ko lambda sy replace kr dia hai.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,14 +1,9 @@
 package com.example.myapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,59 +25,10 @@ public class MainActivity extends AppCompatActivity {
         btn = findViewById(R.id.btn1);
         textView = findViewById(R.id.textView);
 
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                a++;
-                textView.setText(String.valueOf(a));
-            }
+        btn.setOnClickListener(v -> {
+            a++;
+            textView.setText(String.valueOf(a));
         });
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume: ");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause: ");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart: ");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart: ");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy: ");
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
-        super.onSaveInstanceState(outState, outPersistentState);
-        outState.putString("textView",textView.getText().toString());
-        Log.d(TAG, "onSaveInstanceState:workd ");
-
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        textView.setText(savedInstanceState.getString("textView"));
-        Log.d(TAG, "onRestoreInstanceState: ");
-
     }
 }
 
